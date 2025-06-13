10bf9137ea0f9b5fdd7cb57792e81b80825ec7cd
Eric <airkro@qq.com>
2025-06-13 17:10:44 +0800

    chore: export more types

--- lib/index.ts
+++ lib/index.ts
@@ -2,12 +2,12 @@ import { tableGrouping } from './table-grouping.ts';
 import type { Groups } from './table-grouping.ts';
 import { treeInfinity } from './tree-infinity.ts';
 import { treeMapper } from './tree-mapper.ts';
-import type { Mappers } from './tree-mapper.ts';
+import type { Mappers, Tree } from './tree-mapper.ts';
 import type { DataRecord } from './utils.ts';
 
 export { tableGrouping, treeInfinity, treeMapper };
 
-type Options = {
+export type Options = {
   groups?: Groups;
   mapper?: Mappers;
 };
@@ -15,3 +15,5 @@ type Options = {
 export function grouping(data: DataRecord[], { groups, mapper }: Options = {}) {
   return treeMapper(tableGrouping(treeInfinity(data), groups), mapper);
 }
+
+export type { DataRecord, Tree };
